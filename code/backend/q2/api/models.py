af4bfbab7c5761f042b0ae9797f65ddc5540dfb7
Sebastian Otto <git-1@emmrich-otto.de>
2021-03-23 12:38:19 +0100

    Add model 'Link'

--- code/backend/q2/api/models.py
+++ code/backend/q2/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Link(models.Model):
+    id = models.UUIDField(
+        primary_key = True,
+        default = uuid.uuid4,
+        editable = False)
+
+    target = models.URLField()
+    created_at = models.DateTimeField(auto_new_add = True)
+    last_modified_at = models.DateTimeField(auto_new = True)
